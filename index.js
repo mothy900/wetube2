75c81b3e8a8438581d6bd8e4d3f1317b56938d60
mothy900 <mothy900@naver.com>
2020-06-18 23:45:40 +0900

    2.7 Express Core:MiddleWare part two cookir-parser, body-parser, morgan, helmet etc... using lot of middle ware

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookirparser from "cookie-parser";
+import bodyparser from "body-parser";
 const app = express();
 
 const PORT = 4000;
-const handleListening = () => {
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-};
 
-const handleHoem = (req, res) => res.send("Hello form Home");
+const handleHoem = (req, res, next) => res.send("Hello from My ass");
 
-const handleProfile = (req, res) => res.send("You are on my Profile");
+const handleProfile = (req, res, next) => res.send("You are on my Profile");
 
+app.use(cookirparser());
+app.use(bodyparser.json()); //bodyParser.urlencoded 정보를 받는 방법
+app.use(bodyparser.urlencoded({ extended: true })); //bodyParser.urlencoded 정보를 받는 방법
+app.use(helmet());
+app.use(morgan("dev")); //login 흔적을 찾음 middle ware 종류 중 하나
+//app.use(betweenHome); //middle ware
+//////////////////route
 app.get("/", handleHoem); //request
 
 app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
