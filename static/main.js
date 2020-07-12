60f0b8d0546c02bb14a1d7068d75dd12863dc889
mothy900 <mothy900@naver.com>
2020-07-13 00:58:14 +0900

    #5 SCSS

--- static/main.js
+++ static/main.js
@@ -105,7 +105,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scs
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("// removed by extract-text-webpack-plugin\n\n//# sourceURL=webpack:///./assets/scss/styles.scss?");
+eval("throw new Error(\"Module build failed (from ./node_modules/extract-text-webpack-plugin/dist/loader.js):\\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: File to import not found or unreadable: pages/home.scss.\\n        on line 11 of assets/scss/styles.scss\\n>> @import \\\"pages/home.scss\\\";\\r\\n   ^\\n\\n    at D:\\\\work\\\\wetube_new\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:316:20\\n    at D:\\\\work\\\\wetube_new\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:367:11\\n    at D:\\\\work\\\\wetube_new\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:233:18\\n    at context.callback (D:\\\\work\\\\wetube_new\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:111:13)\\n    at Object.callback (D:\\\\work\\\\wetube_new\\\\node_modules\\\\sass-loader\\\\dist\\\\index.js:55:7)\\n    at Object.done [as callback] (D:\\\\work\\\\wetube_new\\\\node_modules\\\\neo-async\\\\async.js:8067:18)\\n    at options.error (D:\\\\work\\\\wetube_new\\\\node_modules\\\\node-sass\\\\lib\\\\index.js:294:32)\");\n\n//# sourceURL=webpack:///./assets/scss/styles.scss?");
 
 /***/ }),
 
