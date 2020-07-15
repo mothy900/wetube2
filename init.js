96afc1021923eca1b3cbb89e24db0bf03c561b3e
mothy900 <mothy900@naver.com>
2020-07-16 01:22:07 +0900

    recording video

--- init.js
+++ init.js
@@ -1,9 +1,11 @@
+import dotenv from "dotenv";
 import "./db";
 import app from "./app";
-import dotenv from "dotenv";
+
 import "./models/Video";
 import "./models/Comment";
 dotenv.config();
+
 const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
